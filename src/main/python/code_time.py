df3aa7fa81d3fb4670e05e34f65d2210508177ff
Sean Martin <seankieran.m@hotmail.com>
2020-05-27 16:48:39 +0100

    An fbs compatible timing program Current version is working

---
+++ src/main/python/code_time.py
@@ -0,0 +1,132 @@
+"""This module holds the code time class."""
+import datetime
+import time
+import os
+import pandas as pd
+
+
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
+
+
+class CodeTime(object):
+
+    def __init__(self, keys=None, default_loc=None):
+        if keys is None:
+            keys = ["Coding", "Reading", "Writing", "Contact", "Misc"]
+        self.time_dict = {}
+        for k in keys:
+            self.time_dict[k] = 0.0
+        self.meta_dict = {"Objective": "Objective:", "Summary": "Summary:"}
+        self.selected = None
+        self.today = datetime.datetime.today().strftime("%d/%m/%Y")
+        self.filename = None
+        self.default_loc = default_loc
+        self.delimeter = "|"
+
+        self.populate()
+
+    def set_file(self, filename):
+        self.filename = filename
+        if os.path.isfile(self.filename):
+            self.load_file()
+
+    def set_selected(self, selected):
+        self.selected = selected
+
+    def set_objective(self, objective):
+        self.meta_dict["Objective"] = objective
+
+    def set_summary(self, summary):
+        self.meta_dict["Summary"] = summary
+
+    def get_time(self):
+        out_time = 0.00
+        if self.selected is not None:
+            out_time = self.time_dict[self.selected]
+        dt = datetime.timedelta(seconds=out_time)
+        return strfdelta(
+            dt, "{hours} hours, {minutes} minutes, {seconds} seconds")
+
+    def populate(self, in_fname=None):
+        if self.default_loc is not None:
+            if os.path.isfile(self.default_loc):
+                with open(self.default_loc, "r") as f:
+                    fname = f.read().strip()
+                    self.set_file(fname)
+        if in_fname is not None:
+            self.set_file(in_fname)
+
+    def stop(self):
+        self.selected = None
+
+    def update(self, elapsed_time):
+        if self.selected is not None:
+            self.time_dict[self.selected] += elapsed_time
+
+    def load_file(self):
+        with open(self.filename, "r") as f:
+            df = pd.read_csv(f, sep=self.delimeter)
+            this_row = df[df["Date"] == self.today]
+            if len(this_row) == 0:
+                return
+            this_row_index = this_row.index[0]
+            for key in this_row:
+                if key == "Date":
+                    continue
+                elif key in list(self.time_dict.keys()):
+                    value = this_row.get(key).at[this_row_index]
+                    self.time_dict[key] = value
+                else:
+                    value = this_row.get(key).at[this_row_index]
+                    self.meta_dict[key] = value
+
+    def save_to_file(self):
+        if os.path.isfile(self.filename):
+            df = pd.read_csv(self.filename, sep=self.delimeter)
+            this_row = df.index[df["Date"] == self.today]
+            if len(this_row) == 0:
+                dict_to_append = self.time_dict
+                dict_to_append.update(self.meta_dict)
+                dict_to_append["Date"] = self.today
+                df = df.append(dict_to_append, ignore_index=True)
+            else:
+                for key, val in self.time_dict.items():
+                    df.at[this_row, key] = val
+                for key, val in self.meta_dict.items():
+                    df.at[this_row, key] = val
+            df.to_csv(self.filename, sep=self.delimeter, index=False)
+
+        else:
+            header_string = "Date" + self.delimeter
+            header_string += self.delimeter.join(list(self.meta_dict.keys()))
+            header_string += self.delimeter.join(list(self.time_dict.keys()))
+            main_string = self.today + self.delimeter
+            main_string += self.delimeter.join(list(self.meta_dict.values()))
+            float_str_l = ["{:.2f}".format(v) for v in self.time_dict.values()]
+            main_string += self.delimeter.join(float_str_l)
+            with open(self.filename, "w") as f:
+                f.write(header_string + "\n")
+                f.write(main_string)
+
+        with open(self.default_loc, "w") as f:
+            f.write(self.filename)
+
+
+if __name__ == "__main__":
+    main_fname = r"E:\Repos\privateCode\CodeTime\test.csv"
+    main_default_loc = r"E:\Repos\privateCode\CodeTime\def.txt"
+    ct = CodeTime(default_loc=main_default_loc)
+    ct.set_selected("Coding")
+
+    start_time = time.monotonic()
+    m_elapsed_time = 0.10
+    while (time.monotonic() - start_time) < 1.0:
+        time.sleep(m_elapsed_time)
+        ct.update(m_elapsed_time)
+
+    ct.stop()
+    ct.save_to_file()
---
+++ src/main/python/main.py
@@ -0,0 +1,204 @@
+import os
+import sys
+import datetime
+
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+from PyQt5 import uic
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import QTimer
+
+from code_time import CodeTime, strfdelta
+
+
+class DesignerUI(object):
+    def __init__(self, design_name):
+        self.appctxt = ApplicationContext()
+        design_path = self.appctxt.get_resource(design_name)
+        Form, Window = uic.loadUiType(design_path)
+        self.window = Window()
+        self.ui = Form()
+        self.ui.setupUi(self.window)
+        self.file_dialog = QFileDialog()
+
+    def start(self):
+        self.window.show()
+        self.exit_code = self.appctxt.app.exec_()
+
+    def getWidgets(self):
+        return self.appctxt.app.allWidgets()
+
+    def getWidgetNames(self):
+        return [w.objectName() for w in self.getWidgets()]
+
+
+class CodeTimeUI(DesignerUI):
+    def __init__(self, design_name):
+        super().__init__(design_name)
+        self.init_vars()
+        self.linkNames()
+        self.setup()
+        self.update_times()
+        self.autosave_timer.start(self.save_frequency)
+
+    def init_vars(self):
+        self.selected_file = None
+        self.should_update = False
+        self.update_frequency_secs = 0.20
+        # Convert to msec for qt timer
+        self.update_frequency = int(self.update_frequency_secs * 1000)
+        self.save_frequency = int(1000 * 120)
+
+    def linkNames(self):
+        self.file_select_button = self.ui.OpenButton
+        self.save_button = self.ui.SaveButton
+        self.quit_button = self.ui.QuitButton
+        self.stop_button = self.ui.StopButton
+
+        self.code_button = self.ui.CodeButton
+        self.contact_button = self.ui.ContactButton
+        self.misc_button = self.ui.MiscButton
+        self.read_button = self.ui.ReadButton
+        self.write_button = self.ui.WriteButton
+
+        self.code_text = self.ui.CodeTimeLine
+        self.contact_text = self.ui.ContactTimeLine
+        self.misc_text = self.ui.MiscTimeLine
+        self.read_text = self.ui.ReadingTimeLine
+        self.write_text = self.ui.WritingTimeLine
+
+        self.time_dict = {
+            "Coding": self.code_text,
+            "Contact": self.contact_text,
+            "Misc": self.misc_text,
+            "Reading": self.read_text,
+            "Writing": self.write_text
+        }
+
+        self.info_text = self.ui.InfoText
+        self.file_select_text = self.ui.FileSelect
+        self.date_text = self.ui.DateLine
+        self.selected_text = self.ui.SelectedLine
+        self.time_text = self.ui.TimeLine
+
+        self.objective_edit = self.ui.ObjectiveEdit
+        self.summary_edit = self.ui.SummaryEdit
+
+    def setup(self):
+        home = os.path.expanduser("~")
+        default_loc = os.path.join(home, ".code_time_skm", "default.txt")
+        os.makedirs(os.path.dirname(default_loc), exist_ok=True)
+        self.code_time = CodeTime(default_loc=default_loc)
+        self.selected_file = self.code_time.filename
+
+        if self.code_time.filename is not None:
+            self.file_select_text.setText(self.code_time.filename)
+        else:
+            self.file_select_text.setText("Please select a file...")
+
+        self.file_select_button.clicked.connect(self.selectFile)
+        self.save_button.clicked.connect(self.save)
+        self.stop_button.clicked.connect(self.stop)
+        self.quit_button.clicked.connect(self.save_quit)
+
+        self.code_button.clicked.connect(self.coding_hit)
+        self.contact_button.clicked.connect(self.contact_hit)
+        self.misc_button.clicked.connect(self.misc_hit)
+        self.read_button.clicked.connect(self.read_hit)
+        self.write_button.clicked.connect(self.write_hit)
+
+        self.date_text.setText(self.code_time.today)
+        self.selected_text.setText("Select a timer")
+        self.time_text.setText("Time will appear here...")
+        self.objective_edit.setPlainText(self.code_time.meta_dict["Objective"])
+        self.summary_edit.setPlainText(self.code_time.meta_dict["Summary"])
+
+        if self.selected_file is None:
+            self.info_text.setText("No file selected, please select one")
+        else:
+            self.info_text.setText(
+                "Selected {}".format(self.code_time.filename))
+
+        self.timer = QTimer(self.window)
+        self.timer.timeout.connect(self.on_update_timer)
+        self.autosave_timer = QTimer(self.window)
+        self.autosave_timer.timeout.connect(self.autosave)
+
+    def selectFile(self):
+        self.selected_file, _filter = self.file_dialog.getSaveFileName(
+            self.window, "Save Output Times", os.path.expanduser("~"),
+            "CSV (*.csv)")
+        self.file_select_text.setText(self.selected_file)
+        self.info_text.setText("Will save to {}".format(self.selected_file))
+        try:
+            self.code_time.set_file(self.selected_file)
+            self.code_time.load_file()
+        except Exception:
+            self.info_text.setText("Selected file could not be parsed")
+            return
+
+    def coding_hit(self):
+        self.time_button_hit("Coding")
+
+    def contact_hit(self):
+        self.time_button_hit("Contact")
+
+    def misc_hit(self):
+        self.time_button_hit("Misc")
+
+    def read_hit(self):
+        self.time_button_hit("Reading")
+
+    def write_hit(self):
+        self.time_button_hit("Writing")
+
+    def time_button_hit(self, button_type):
+        if self.code_time.selected == button_type:
+            self.stop()
+        else:
+            self.code_time.set_selected(button_type)
+            self.selected_text.setText("Timing " + button_type)
+            self.time_text.setText(self.code_time.get_time())
+            self.timer.start(self.update_frequency)
+
+    def stop(self):
+        self.timer.stop()
+        self.selected_text.setText("Select a timer")
+        self.time_text.setText("Time will appear here...")
+        self.code_time.set_selected(None)
+
+    def autosave(self):
+        if self.code_time.filename is not None:
+            self.code_time.save_to_file()
+            self.info_text.setText("Successfully autosaved to {} at {}".format(
+                self.code_time.filename,
+                datetime.datetime.now().strftime("%H:%M:%S")))
+
+    def save(self):
+        self.code_time.set_objective(self.objective_edit.toPlainText())
+        self.code_time.set_summary(self.summary_edit.toPlainText())
+        self.code_time.save_to_file()
+        self.info_text.setText("Successfully saved to {}".format(
+            self.code_time.filename))
+
+    def save_quit(self):
+        self.save()
+        self.appctxt.app.quit()
+
+    def update_times(self):
+        for key, val in self.code_time.time_dict.items():
+            dt = datetime.timedelta(seconds=val)
+            self.time_dict[key].setText(
+                strfdelta(
+                    dt, "{hours} hours, {minutes} minutes, {seconds} seconds"))
+        self.time_text.setText(self.code_time.get_time())
+
+    def on_update_timer(self):
+        self.code_time.update(self.update_frequency_secs)
+        self.update_times()
+
+
+if __name__ == "__main__":
+    here = os.path.dirname(os.path.realpath(__file__))
+    ui = CodeTimeUI("codetime.ui")
+    ui.start()
+    sys.exit(ui.exit_code)
